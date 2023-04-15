5b1cfce9d55f3a9c57fa6da31b923002785a7a14
Motaz Omar <motaz.o821@gmail.com>
2023-04-15 11:06:09 +0200

    launch

---
+++ main.js
@@ -0,0 +1,25 @@
+fetch(
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8butISFwT-Wl7EV0hUK0BQ&order=date&key=AIzaSyC5BilnxYvPKTDdIMOhovzQpHHQlnPjVF0"
+)
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    let videos = data.items;
+    let videoContainez = document.querySelector("#main");
+    for (video of videos) {
+      videoContainez.innerHTML += `
+      <article class="video">
+      
+      
+      
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/${video.id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div class="details">
+    <h1 class="title">${video.snippet.title}</h1>
+      <p class="description">${video.snippet.description}</p>
+      </div>
+    </article>
+
+    `;
+    }
+  });
